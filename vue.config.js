81fab531efd089d9377ba95e05b4ef37db2d7ef3
서정욱 <53929455+boyseos@users.noreply.github.com>
2020-02-04 12:32:20 +0900

    2-4 합치기전
    올라가라

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  "outputDir": "../src/main/resources/static",
-  "indexPath": "../static/index.html",
+  "outputDir": "./public/static",
+  "indexPath": "./public/index.html",
   "devServer": {
     //"proxy": "http://localhost:8080"
     "proxy": "http://15.165.26.82"
