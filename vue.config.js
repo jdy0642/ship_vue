c05dea7a305b1d91211f03fa3b563811e00865e9
jdy0642 <jdy0642@gmail.com>
2020-02-04 15:21:25 +0900

    1
    1

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ module.exports = {
   "outputDir": "./public/static",
   "indexPath": "./public/index.html",
   "devServer": {
-    //"proxy": "http://localhost:8080"
-    "proxy": "http://15.165.26.82"
+    "proxy": "http://localhost:8080"
+    //"proxy": "http://15.165.26.82"
   },
   "transpileDependencies": [
     "vuetify"
