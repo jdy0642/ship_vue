9c1cbae8b2348d8e13f317474c2fbe3d644fde0c
boyseos <boyseos@gmail.com>
2020-02-16 20:31:50 +0900

    마이페이지 예약일자 경기일자로 변경

--- src/components/auth/MyPage.vue
+++ src/components/auth/MyPage.vue
@@ -105,7 +105,7 @@
           <v-card style="margin:3%;" width="13%">
             <br>
                 <p class="text-break" style="max-width: 4rem;">
-                  {{timeToDate(item.resdate)}}
+                  {{timeToDate(item.futsal.time)}}
                 </p>
           </v-card >
           <v-card style="margin:3%" class="title font-weight-light" width="33%" >
